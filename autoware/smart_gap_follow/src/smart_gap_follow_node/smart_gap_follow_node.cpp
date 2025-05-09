bfd3a35feaa799997d08f318e0032f80ae289991
pojenwang <pw.pjwang@gmail.com>
2025-05-09 16:21:10 -0700

    parameter tuning for faster lap time

--- autoware/smart_gap_follow/src/smart_gap_follow_node/smart_gap_follow_node.cpp
+++ autoware/smart_gap_follow/src/smart_gap_follow_node/smart_gap_follow_node.cpp
@@ -356,13 +356,14 @@ void SmartGapFollowNode::calcMotionCmd(const sensor_msgs::msg::LaserScan & scan)
 
   target_speed = 0.5 * target_speed_gap + 0.5 * target_speed_steer;
 
-  if(dist_to_gap < 1.1 && abs(steer_angle) * 180 / M_PI > 25){
+  if((dist_to_gap < 1.1 && abs(steer_angle) * 180 / M_PI > 25) 
+    || (dist_to_gap < 1.45 && gap_size < 2.5 && gap_angle * 180 / M_PI < -120 && gap_angle * 180 / M_PI > -150)){
     target_speed = min_speed;
     RCLCPP_INFO_STREAM(get_logger(), "more slow!!!");
   }
-  if(dist_to_gap > boost_dist_thresh && abs(steer_angle) * 180 / M_PI < 15){
+  if(dist_to_gap > boost_dist_thresh){// && abs(steer_angle) * 180 / M_PI < 20){
     target_speed = boost_speed;
-    acceleration *= 2;
+    acceleration *= 4;
     RCLCPP_INFO_STREAM(get_logger(), "boost!!!");
   }
 
